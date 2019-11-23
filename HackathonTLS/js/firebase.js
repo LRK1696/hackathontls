2f66796cd9ca6a5d9b1af25f2112e544301a40a5
renattoyague <48736281+LRK1696@users.noreply.github.com>
2019-11-23 02:32:31 -0500

    Add files via upload

---
+++ HackathonTLS/js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+            apiKey: "AIzaSyBLMrcIL2PC3gv59M8nG4eX9IbOT8FX5SI",
+            authDomain: "hackathontls.firebaseapp.com",
+            databaseURL: "https://hackathontls.firebaseio.com",
+            projectId: "hackathontls",
+            storageBucket: "hackathontls.appspot.com",
+            messagingSenderId: "345440720165",
+            appId: "1:345440720165:web:4f9f5e90d2ba09e8c1b6cc"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
